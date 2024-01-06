25997d604bf8b74dabf044068b7f18b77352a591
Yehyia Aljuboory <Yehyia Aljuboory@DESKTOP-JNNUE2G>
2024-01-05 18:03:31 -0600

    initial project 1

---
+++ src/demo/TestDemo.java
@@ -0,0 +1,21 @@
+package demo;
+
+public class TestDemo {
+
+public static void main(String[] args) {
+	System.out.println("Welcome to Test 1");
+	
+	
+	System.out.println("Push Back this message and change (A) to B");
+	
+	
+	//Do it Below. 
+	//Regards, Yahiya.. :)
+	
+	
+	
+	
+	
+	
+}
+}
